6f04202b474a3e3f318ef45ed630835ce18483f4
Dan DiMartino <dandimartino@gmail.com>
2021-02-05 17:11:16 -0500

    added anchor tag for github repo

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,7 @@ const Home = () => {
     
     return (
         <div>
+            <a href="https://github.com/dldimartino/kapitus">Click for Github Repo</a>
             <SearchForm />
         </div>
     )
